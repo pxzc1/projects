6fddc6b3239c1dd0a9de9cda4396641ccedd90ca
Phattaraphan <tonaok2555@gmail.com>
2026-01-07 18:37:36 +0700

    edit flake.nix -> nixos-release-24.05 (env) and add .gitignore, add _blank.py (for testing)

--- flake.nix
+++ flake.nix
@@ -1,21 +1,29 @@
 {
-  description = "Python 3.10.13 dev shell";
+  description = "Python 3.10 development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.python310
-          pkgs.python310Packages.pip
-          pkgs.python310Packages.virtualenv
-        ];
-      };
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    python310 = pkgs.python310;
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        python310
+        python310.pkgs.venvShellHook
+        python310.pkgs.pip
+      ];
+
+      venvDir = "env";
+
+      postShellHook = ''
+        echo "Using Python: $(python --version)"
+      '';
     };
+  };
 }
