0bae42d1165053c7a6b8ec889a39647458bf870d
Phattaraphan <tonaok2555@gmail.com>
2026-01-07 21:50:50 +0700

    rename test/_blank.py -> verification.py (under same dir), configured shell.nix (local)

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    python310
+    stdenv.cc.cc
+    glibc
+    cudaPackages.cudatoolkit
+  ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.linuxPackages.nvidia_x11}/lib:$LD_LIBRARY_PATH
+  '';
+}
+
+#nix-shell --impure first before running to expose CUDA runtime visibility [ChatGPT]
---
+++ test/verification.py
@@ -0,0 +1,6 @@
+import torch
+
+print('PyTorch...')
+print(f'Version: {torch.__version__}')
+print(f'CUDA Version: {torch.torch.version.cuda}')
+print(f'CUDA Available: {torch.cuda.is_available()}\n')
