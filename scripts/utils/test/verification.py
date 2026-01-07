b8d5633c064ea4a113a912a16026a2c7aec8f9bb
Phattaraphan <tonaok2555@gmail.com>
2026-01-07 22:42:59 +0700

    bugs commit

--- scripts/utils/test/verification.py
+++ scripts/utils/test/verification.py
@@ -1,6 +1,7 @@
 import torch
 
-print('PyTorch...')
-print(f'Version: {torch.__version__}')
-print(f'CUDA Version: {torch.torch.version.cuda}')
-print(f'CUDA Available: {torch.cuda.is_available()}\n')
+def verify() -> str:
+    print('PyTorch...')
+    print(f'Version: {torch.__version__}')
+    print(f'CUDA Version: {torch.torch.version.cuda}')
+    print(f'CUDA Available: {torch.cuda.is_available()}\n')
