baa548d5648cce3bbf891466e32152df33c01793
phattaraphan <tonaok255@gmail.com>
2026-01-10 01:01:12 +0700

    redefine shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,27 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  # Define python with the necessary packages for Jupyter/Kernelspec
+  myPython = pkgs.python310.withPackages (ps: with ps; [
+    ipykernel
+    jupyter-client
+    # Add ML packages here so they see CUDA
+  ]);
+in
 pkgs.mkShell {
   packages = with pkgs; [
-    python310
+    myPython
     stdenv.cc.cc
     glibc
     cudaPackages.cudatoolkit
   ];
 
   shellHook = ''
-    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.linuxPackages.nvidia_x11}/lib:$LD_LIBRARY_PATH
+    # Fix for libstdc++.so.6 and CUDA visibility
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.cudaPackages.cudatoolkit}/lib:$LD_LIBRARY_PATH
+    
+    # Optional: Automatically register the kernel for this shell session
+    # python -m ipykernel install --user --name=nixos-cuda-env --display-name "Python 3.10 (NixOS + CUDA)"
   '';
 }
 
